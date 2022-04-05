4a27a31140d559bc6cbb965cfaace01fe9305dbe
Mihir <67392640+mihir2310@users.noreply.github.com>
2022-04-05 01:55:56 -0400

    Create Multiply_strings.py
    Took the easy way out (wasn't even allowed to) but did it anyways.

---
+++ Multiply_strings.py
@@ -0,0 +1,6 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        intnum1 = int(num1)
+        intnum2 = int(num2)
+        answerint = intnum1*intnum2
+        return str(answerint)
